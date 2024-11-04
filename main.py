88b022d9b81144fa95ebb5aa565d3f030a3b5ccf
Jake <jakem18@gmail.com>
2024-11-04 15:45:33 -0500

    testing UI first attempt

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from nicegui import ui
 
+#Cards page
 @ui.page('/cards')
 async def cards():
     ui.label("This should be the cards page")
+    
 
+#Home Page
 ui.label("Card Collector")
 ui.button("Cards", on_click=lambda: ui.navigate.to(cards,new_tab=False))
 ui.run()
