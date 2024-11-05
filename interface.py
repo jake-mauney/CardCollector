4bdc140dc1d195b610cb590f13560248e6486722
Jake <jakem18@gmail.com>
2024-11-05 12:52:53 -0500

    Shell complete. Added functional quit option to file menu

--- interface.py
+++ interface.py
@@ -7,9 +7,30 @@ headerLabel = tk.Label(text=HeaderTxt)
 
 #Buttons
 CardViewButton = tk.Button(root, text="View Collection")
+DeckViewButton = tk.Button(root, text="Decks")
+PriceUpdateButton = tk.Button(root, text="Price Update")
+
+#Top Menu
+menubar = tk.Menu(root)
+#Creates "File" var for the menu
+filemenu = tk.Menu(menubar, tearoff=0)
+#Creates the "Import Cards option"
+filemenu.add_command(label="Import Cards")
+filemenu.add_command(label="Quit" ,command=root.quit)
+#Sets the file option in the menu
+menubar.add_cascade(label="File", menu=filemenu)
+#puts the menu on the screen
+root.config(menu=menubar)
 
 
+#Pack header
 headerLabel.pack()
+
+#Pack buttons
 CardViewButton.pack()
+DeckViewButton.pack()
+PriceUpdateButton.pack()
+
+#wait on something
 root.mainloop()
 
