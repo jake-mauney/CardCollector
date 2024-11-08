122d527e23559bd6654eb3314e86d6941b2c456f
Jake <jakem18@gmail.com>
2024-11-07 19:41:23 -0500

    Different error now. Getting GitHub up to date after what ive done.

--- updateprice.py
+++ updateprice.py
@@ -5,21 +5,21 @@ from datetime import datetime
 
 def updateprice(setcode, setnum, idCard, foil):
     if foil == 'false':
+        c = DBconnect.DBConnect()
+        cursor = c.cursor()
         reqeust_url = "https://api.scryfall.com/cards/"+setcode.lower()+"/"+setnum
         response = requests.get(reqeust_url)
         data = response.json()
-        print(data['prices']['usd'])
-        
-        c = DBconnect.DBConnect()
-        cursor = c.cursor()
         values = [data['prices']['usd'],datetime.today().strftime('%Y-%m-%d'),idCard]
-        cursor.execute("INSERT INTO Price (price,date,RelCard_Id) VALUES (%s, %s, %s)",(values))
+        cursor.execute("INSERT INTO CardCollector.Price (price,date,RelCard_Id) VALUES (%s, %s, %s)",(values))
         c.commit
+        
     else:
         reqeust_url = "https://api.scryfall.com/cards/"+setcode+"/"+setnum
         response = requests.get(reqeust_url)
         data = response.json()
         print(data['prices']['usd_foil'])
+        print("You did it wrong")
 
 if __name__ == "__main__":
     updateprice()
