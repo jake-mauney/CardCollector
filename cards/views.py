cd75db136dea101b7e988b142cbc64ea0a9314b0
jake.mauney <jakem18@gmail.com>
2024-11-15 12:22:48 -0500

    cleaned up detail view for cards

--- cards/views.py
+++ cards/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Card
+from decks.models import Deck, DeckEntry
+from django.db.models import Count
 
 def index(request):
     card_collection = Card.objects.all() #.all is what made this work and grab all cards
@@ -9,10 +11,6 @@ def index(request):
 
 def detail(request, card_id):
     card = Card.objects.get(id = card_id)
-    card_name=card.name
-    like_cards=Card.objects.filter(name=card_name).exclude(pk=card_id)
-    card_set=card.set_code
-    set_num = card.set_num
-    foil=card.foil
-    context = {"card_name":card_name, "card_set":card_set, "foil":foil, "set_num":set_num, "likeCollection":like_cards}  #passes the card name that I need to display. can comma seperate no problem. The quotes are what you referencein the html
+    like_cards=Card.objects.filter(name=card.name).exclude(pk=card_id)
+    context = {"card_name":card.name, "card_set":card.set_code, "foil":card.foil, "set_num":card.set_num, "likeCollection":like_cards}  #passes the card name that I need to display. can comma seperate no problem. The quotes are what you referencein the html
     return render(request, "cards/detail.html", context) #tells what template to use for the specified url "detail"
