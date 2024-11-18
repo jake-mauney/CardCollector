9fc191f9733722ce0f7a107764c49e5da965b301
Jake <jakem18@gmail.com>
2024-11-17 19:36:12 -0500

    updated the html templates to look at base.html in siteutils

--- decks/views.py
+++ decks/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from .models import Deck, DeckEntry
-from django.http import HttpResponse #just for testing
+from siteutils.models import menu_items
 
 def index(request):
     deck_collection = Deck.objects.all() #.all is what made this work and grab all cards
-    context = {"collection": deck_collection}
+    items = menu_items.objects.all()
+    context = {"collection": deck_collection, "items": items}
     return render(request, "decks/index.html", context)
 
 def detail(request, deck_id):
     entry_collection = DeckEntry.objects.filter(rel_deck=deck_id)
+    items = menu_items.objects.all()
     deck = Deck.objects.get(pk=deck_id)
-    context = {"collection": entry_collection, "deck": deck}
+    context = {"collection": entry_collection, "deck": deck, "items": items}
     return render(request, "decks/detail.html", context)
 
 
