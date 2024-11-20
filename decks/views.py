c9af1eb07d92fb9742ecd69565f4f03919f1e6ca
jake.mauney <jakem18@gmail.com>
2024-11-19 21:55:05 -0500

    cleaned deck view to start agg the cards, added import to the admin site

--- decks/views.py
+++ decks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import Deck, DeckEntry
+from .models import Deck, DeckEntry, Card
 from siteutils.models import menu_items
+from django.db.models import Count
+from django.db.models import Subquery
 
 def index(request):
     deck_collection = Deck.objects.all() #.all is what made this work and grab all cards
@@ -10,9 +12,11 @@ def index(request):
 
 def detail(request, deck_id):
     entry_collection = DeckEntry.objects.filter(rel_deck=deck_id)
+    card_collection = Card.objects.filter(id__in=Subquery(DeckEntry.objects.filter(rel_deck=deck_id).values('id')))
+    #loop through collection, if name exsist in list then up by one, if not then create/append
     items = menu_items.objects.all()
     deck = Deck.objects.get(pk=deck_id)
-    context = {"collection": entry_collection, "deck": deck, "items": items}
+    context = {"collection": card_collection, "deck": deck, "items": items}
     return render(request, "decks/detail.html", context)
 
 
