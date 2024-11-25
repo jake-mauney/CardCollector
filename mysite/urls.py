d74f61a0b46e738cac8fcae4b9c0fd7ad61b5779
Jake <jakem18@gmail.com>
2024-11-25 18:33:19 -0500

    created user import folder for when we import csvs

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,11 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cards/', include("cards.urls")),
     path('decks/', include("decks.urls")),
     path('import', include('cardImport.urls')),
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    
 ]
+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
