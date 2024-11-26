10dd5b0f9a6b38c257d86e23e197e4158b1ed552
Jake <jakem18@gmail.com>
2024-11-25 22:25:11 -0500

    logic to run after importrequest is created.

--- cardImport/models.py
+++ cardImport/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 status_options = [('NEW', 'new'), ('IN PROCESS', 'In Process'), ("DONE", "Done"), ("ERROR", "Error")]
 type_options = [('CARD', 'Card'), ('DECK', 'deck')]
 class ImportRequest(models.Model):
@@ -7,3 +10,13 @@ class ImportRequest(models.Model):
     status = models.CharField(max_length= 10, choices=status_options)
     error_msg = models.CharField(max_length=10000)
     type = models.CharField(max_length=10, choices=type_options)
+
+    
+@receiver(post_save, sender=ImportRequest) #listens for when a import request is created
+def model_instance_created(sender, instance, created, **kwargs): 
+    if created:  #if created
+        reqeustRecord = ImportRequest.objects.get(pk=instance.pk)
+        reqeustRecord.status = 'DONE' #set the status to done
+        reqeustRecord.save() #actually save the record
+    else:
+        print("something went wrong")
