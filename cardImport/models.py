0e0d1511a169929714b05856974c2dd732b51681
Jake <jakem18@gmail.com>
2024-12-09 11:33:09 -0500

    can now create import reqeust record from outside admin.

--- cardImport/models.py
+++ cardImport/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+
 #the picklist options referencd below. Not sure why it is in the format of SOMETHING, something
 status_options = [('NEW', 'new'), ('IN PROCESS', 'In Process'), ("DONE", "Done"), ("ERROR", "Error")]
 type_options = [('CARD', 'Card'), ('DECK', 'deck')]
 
 class ImportRequest(models.Model):
-    request_date = models.DateTimeField() #Just to keep track of when these are created and how long to process
+    request_date = models.DateTimeField(auto_now_add=True) #Just to keep track of when these are created and how long to process
     csv_file = models.FileField() #the actual csv file. No validation 
     status = models.CharField(max_length= 10, choices=status_options) #used in case error handling is implemented
     error_msg = models.CharField(max_length=10000) 
--- cardImport/views.py
+++ cardImport/views.py
@@ -6,9 +6,10 @@ from siteutils.models import menu_items
 
 def importpage(request):
     items = menu_items.objects.all() #menu items
-    form = CreateImport(request.POST or None) #Get the form to render on the page
+    today = datetime.datetime.now()
+    form = CreateImport(request.POST,request.FILES, initial={'request_date': today}) #Get the form to render on the page
     context = {"form": form, "items": items}
     if request.method == 'POST' and form.is_valid():
         #actually create the record
-        newImport = ImportRequest.objects.create(request_date = datetime.datetime.now(), csv_file = form.data('csv_file'), type = form.cleaned_data('type')) 
+        form.save()
     return render(request, 'cardImport/Import.html', context)
