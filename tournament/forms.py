56a876920342d8293d74e8acabf1fd07c1b0feb5
Jake <jakem18@gmail.com>
2024-12-08 20:27:08 -0500

    added menu items to tournament, got register page to respect the page you clicked register on.

--- tournament/forms.py
+++ tournament/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Tournament
+from .models import Tournament, Registration
 from django.forms import ModelForm
 
 class TournamentCreateForm(ModelForm):
     class Meta:
         model = Tournament
         fields = ['title','game','format']
+
+class RegisterTournament(ModelForm):
+    class Meta:
+        model = Registration
+        fields = [ 'player', 'deck']
--- tournament/views.py
+++ tournament/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
-from .forms import TournamentCreateForm
+from .forms import TournamentCreateForm, RegisterTournament
 from .models import Tournament, Registration, Match
+from siteutils.models import menu_items
 
 def TourCreate(request):
-
+    items = menu_items.objects.all()
     form = TournamentCreateForm(request.POST or None)
-    context = {"form": form}
+    context = {"form": form, "items": items}
     return render (request, "tournament/createtour.html", context)
 
 def TourHome(request):
     TourList = Tournament.objects.all()
+    items = menu_items.objects.all()
     form = TournamentCreateForm(request.POST or None)
-    context = {"TourList": TourList, "form": form}
+    context = {"TourList": TourList, "form": form, "items": items}
     #what to do when POST, new tournament form handler
     if request.method == 'POST' and form.is_valid():
         #sets the values
@@ -23,12 +25,21 @@ def TourHome(request):
     return render (request, "tournament/tourhome.html", context)
 
 def TourDetails(request, tour_id):
+    items = menu_items.objects.all()
     tournament = Tournament.objects.get(pk=tour_id)
     RegPlayers = Registration.objects.filter(tournament = tour_id)
-    context = {"Tournament": tournament, "Players": RegPlayers}
+    context = {"Tournament": tournament, "Players": RegPlayers, "items": items}
     return render(request, "tournament/tourdetail.html", context)
 
 def Register(request, tour_id):
+    items = menu_items.objects.all()
     tournament = Tournament.objects.get(pk=tour_id)
-    context = {"Tournament": tournament}
+    form = RegisterTournament(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        #sets the values
+        player_form = form.cleaned_data['player']
+        deck_form = form.cleaned_data['deck']
+        #actually create the record
+        newReg = Registration.objects.create(player=player_form, tournament = tournament, deck = deck_form  )
+    context = {"Tournament": tournament, "form": form, "items": items}
     return render(request, "tournament/register.html", context)
