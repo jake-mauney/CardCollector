8a10bcd593fa5e5f18349bbf2cd88a92dd6c5a7f
jake.mauney <jakem18@gmail.com>
2024-12-12 15:16:48 -0500

    Cleaned up deck detail view to respect the deck entry records. added page title to base template.

--- cards/views.py
+++ cards/views.py
@@ -7,13 +7,14 @@ from siteutils.models import menu_items
 
 def index(request):
     items = menu_items.objects.all()
-    card_collection = Card.objects.all() #.all is what made this work and grab all cards
-    context = {"collection": card_collection, "items": items}
+    card_collection = Card.objects.filter(owner=request.user) #.all is what made this work and grab all cards
+    context = {"collection": card_collection, "items": items, "PageTitle": 'Your Collection'}
     return render(request, "cards/index.html", context)
 
 def detail(request, card_id):
     card = Card.objects.get(id = card_id)
     like_cards=Card.objects.filter(name=card.name).exclude(pk=card_id)
     items = menu_items.objects.all()
-    context = {"card_name":card.name, "card_set":card.set_code, "foil":card.foil, "set_num":card.set_num, "likeCollection":like_cards, "items": items}  #passes the card name that I need to display. can comma seperate no problem. The quotes are what you referencein the html
+    PageTitle = card.name +' Details'
+    context = {"card_name":card.name, "card_set":card.set_code, "foil":card.foil, "set_num":card.set_num, "likeCollection":like_cards, "items": items, "PageTitle": PageTitle}  #passes the card name that I need to display. can comma seperate no problem. The quotes are what you referencein the html
     return render(request, "cards/detail.html", context) #tells what template to use for the specified url "detail"
--- decks/views.py
+++ decks/views.py
@@ -8,16 +8,18 @@ from django.db.models import Subquery
 def index(request):
     deck_collection = Deck.objects.all() #.all is what made this work and grab all cards
     items = menu_items.objects.all()
-    context = {"collection": deck_collection, "items": items}
+    PageTitle = 'Your Decks'
+    context = {"collection": deck_collection, "items": items, "PageTitle": PageTitle}
     return render(request, "decks/index.html", context)
 
 def detail(request, deck_id):
     entry_collection = DeckEntry.objects.filter(rel_deck=deck_id)
-    card_collection = Card.objects.filter(id__in=Subquery(DeckEntry.objects.filter(rel_deck=deck_id).values('id')))
+    card_collection = Card.objects.filter(id__in=Subquery(DeckEntry.objects.filter(rel_deck=deck_id).values('rel_card')))
     #loop through collection, if name exsist in list then up by one, if not then create/append
     items = menu_items.objects.all()
     deck = Deck.objects.get(pk=deck_id)
-    context = {"collection": card_collection, "deck": deck, "items": items}
+    PageTitle = deck.name + ' Details'
+    context = {"collection": card_collection, "deck": deck, "items": items, "PageTitle": PageTitle}
     return render(request, "decks/detail.html", context)
 
 
